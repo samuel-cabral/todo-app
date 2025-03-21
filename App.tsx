e3ecacc938301bf0299f7f8d5b20d863e95b79cb
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-03-21 15:47:43 -0300

    feat: implement dark theme and add custom typography and color themes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
+import {
+  Inter_400Regular as InterRegular,
+  Inter_700Bold as InterBold,
+  useFonts,
+} from '@expo-google-fonts/inter'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
+import { theme } from './src/theme'
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    InterRegular,
+    InterBold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Samuel!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Todo App</Text>
+      <StatusBar style="light" />
     </View>
   )
 }
@@ -13,8 +29,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.colors['gray-700'],
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontFamily: theme.typography.fontFamily.bold,
+    fontSize: theme.typography.fontSize.lg,
+    color: theme.colors['gray-100'],
   },
 })
---
+++ src/theme/typography.ts
@@ -0,0 +1,12 @@
+export const typography = {
+  fontFamily: {
+    regular: 'InterRegular',
+    bold: 'InterBold',
+  },
+  fontSize: {
+    sm: 12,
+    md: 14,
+    lg: 16,
+  },
+  lineHeight: 1.4, // 140%
+}
